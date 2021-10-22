0b364c63b2ace17450d5c257ce7910f7640137af
adaich <86274265+adaich@users.noreply.github.com>
2021-10-22 12:03:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfoLYSJs8PsihvcKa8gcgxk-VBvQN6DzU",
+  authDomain: "app-326ef.firebaseapp.com",
+  projectId: "app-326ef",
+  storageBucket: "app-326ef.appspot.com",
+  messagingSenderId: "235679754804",
+  appId: "1:235679754804:web:43ffbfc0ca8d3cc9463861"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ----------------------------------------------------------------------
+
+function OrderMessageMaker() {
+    const formEl = document.querySelector(".order-form")
+
+}
+
+function main() {
+    
+}
+
